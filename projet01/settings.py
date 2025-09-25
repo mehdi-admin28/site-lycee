5f554aa3cfce401624ca71df8afb54119ff16dec
mehdi-admin28 <mehdi.chek6514@gmail.com>
2025-09-25 22:40:41 +0100

    Fix cache location for Render

--- projet01/settings.py
+++ projet01/settings.py
@@ -185,6 +185,6 @@ CACHES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/var/tmp/django_cache',
+        'LOCATION':  os.path.join(BASE_DIR, 'cache',
     }
 }
