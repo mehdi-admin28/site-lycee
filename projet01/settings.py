b23540fc7a7981df028680981cc764ff6c5fda15
mehdi-admin28 <mehdi.chek6514@gmail.com>
2025-09-26 14:08:11 +0100

    fin

--- projet01/settings.py
+++ projet01/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # static images c est moi pas django donc peux supprimer
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
-#ana li zdt hada f taht (en bah)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
