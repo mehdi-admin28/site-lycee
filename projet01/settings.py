77e19969dda2aa13996b7084df719986c297879a
mehdi-admin28 <mehdi.chek6514@gmail.com>
2025-10-07 21:43:37 +0100

    maj static et fichiers

--- projet01/settings.py
+++ projet01/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import dj_database_url
 import os
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
